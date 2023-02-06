90273cb904e467d4a267239d23923436b1498cac
Alan M. Souza <alan.graia@hotmail.com>
2023-02-06 15:45:59 -0300

    Secao 3 exercicio

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -2,10 +2,23 @@
 
 /// Criando uma suite de testes
 describe('Central de Atendimento ao Cliente TAT', function() {
+    beforeEach(function(){
+        cy.visit('./src/index.html'); ///visita uma pagina
+    })
     ///criando um caso de teste
     it('verifica o título da aplicação', function() {
-        cy.visit('./src/index.html'); ///visita uma pagina
         cy.title().should('be.equal', 'Central de Atendimento ao Cliente TAT') /// titulo da pagina deve ser igual a Central de Atendimento ao Cliente TAT
+    })
+
+    it('preenche os campos obrigatórios e envia o formulário', function() {
+        cy.get('#firstName').type('Alan')
+        cy.get('#lastName').type('Moreira de Souza')
+        cy.get('#email').type('alan@gmail.com')
+        cy.get('#open-text-area').type('Testando textarea')
+        cy.get('#phone-checkbox').click
+        cy.get('#phone').type('1234')
+        cy.get('button[type="submit"]').click()
+        cy.get('.success > strong').should('be.visible')
     })
   })
    
