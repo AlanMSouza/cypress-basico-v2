aa4bc0824961f55c0bde97da984905a845b38b8b
Alan M. Souza <alan.graia@hotmail.com>
2023-02-06 15:01:41 -0300

    secao 2

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -1,9 +1,11 @@
 /// <reference types="Cypress" />
 
+/// Criando uma suite de testes
 describe('Central de Atendimento ao Cliente TAT', function() {
+    ///criando um caso de teste
     it('verifica o título da aplicação', function() {
-        cy.visit('./src/index.html');
-        cy.title().should('be.equal', 'Central de Atendimento ao Cliente TAT')
+        cy.visit('./src/index.html'); ///visita uma pagina
+        cy.title().should('be.equal', 'Central de Atendimento ao Cliente TAT') /// titulo da pagina deve ser igual a Central de Atendimento ao Cliente TAT
     })
   })
-  
+   
